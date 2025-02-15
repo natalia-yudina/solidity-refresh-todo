512291841d8de7e323e573ca586724a429a174ee
Natalia Iudina <78476252+natalia-yudina@users.noreply.github.com>
2025-02-15 07:37:32 +0000

    create a todo

--- todos-frontend/src/App.tsx
+++ todos-frontend/src/App.tsx
@@ -1,46 +1,52 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { useWatchTodosTodoCreated, useWatchTodosTodoDeleted, useWatchTodosTodoUpdated, useWriteTodosCreateTodo, useWriteTodosDeleteTodo, useWriteTodosUpdateTodo } from './generated.ts'
+import { type FormEvent, useState } from 'react';
+import { config } from './wagmi.ts';
+import type { Address } from 'viem';
 
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+const contractAddress: Address = "0x5a13b12ddd9c1133312bd78587d1d0c021e96b98";
 
-  return (
-    <>
-      <div>
-        <h2>Account</h2>
+function CreateTodoForm() {
+	useWatchTodosTodoCreated({
+    	config: config,
+    	address: contractAddress,
+    	onLogs(logs) {
+        	console.log('Event says "created a todo"', logs)
+    	},
+	});
+	const [todoTitle, setTodoTitle] = useState<string>("");
+	const [todoDescription, setTodoDescription] = useState<string>("");
+	const { writeContractAsync } = useWriteTodosCreateTodo(
+    	{
+        	config: config
+    	}
+	);
 
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
+	const submitCreateTodo = async (e: FormEvent
+	) => {
+    	e.preventDefault();
 
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
+    	await writeContractAsync({
+        	address: contractAddress,
+        	args: [todoTitle, todoDescription]
+    	});
+    	setTodoTitle("");
+    	setTodoDescription("");
+	}
 
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+	return (<>
+    	<form onSubmit={(e) => submitCreateTodo(e)}>
+        	<input type="text" name="todo-title" value={todoTitle} placeholder='Todo Title' onChange={(e) => setTodoTitle(e.target.value)} />
+        	<textarea
+            	rows={5}
+            	name="todo-description"
+            	placeholder="What's on your mind?"
+            	value={todoDescription}
+            	onChange={(e) =>
+                	setTodoDescription(e.target.value)
+            	}
+        	/>
+        	<button type="submit">Submit New Todo</button>
+    	</form>
+	</>)
 }
-
-export default App
